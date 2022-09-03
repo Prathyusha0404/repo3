66c0443d120cfde2763f3590ef3298d79e09e2a3
Uppugandla,Prathyusha <pathu0404@gmail.com>
2022-09-03 21:19:59 +0000

    new update

--- file22.java
+++ file22.java
@@ -2,7 +2,7 @@ import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;  
 public class CurrentDateTimeExample1 {  
   public static void main(String[] args) {  
-   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+   DateTimeFormatter dtf = DateTimeFormatter.ofPattern("2022/09/03 HH:mm:ss");
    LocalDateTime now = LocalDateTime.now();
    System.out.println(dtf.format(now));
   }  
